9c18b32aae6292d923ae5bc92bd5fb5c83f8d246
imitrichev <imitrichev@muctr.ru>
2022-12-16 18:09:36 +0300

    added tests

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include "gtest/gtest.h"
 
 //2. y=x^3 cos(x) - e^(2x)
 
@@ -19,9 +20,28 @@ double y(double x)
 	return  addend1(x)-addend2(x);
 }
 
+TEST(FunctionTesting, test_addend1) {
+  EXPECT_DOUBLE_EQ(addend1(0),0.0);
+  EXPECT_DOUBLE_EQ(addend1(1),0.999847695);
+  EXPECT_DOUBLE_EQ(addend1(-1),−0.999847695);
+}
+
+TEST(FunctionTesting, test_addend2) {
+  EXPECT_DOUBLE_EQ(addend2(0),1.0);
+  EXPECT_DOUBLE_EQ(addend2(1),7.38905609893);
+  EXPECT_DOUBLE_EQ(addend2(-1),0.13533528323);
+}
+
+TEST(FunctionTesting, test_y){
+  EXPECT_DOUBLE_EQ(y(0),-1.0);
+  EXPECT_DOUBLE_EQ(y(1),0.999847695-7.38905609893);
+  EXPECT_DOUBLE_EQ(y(-1),−0.999847695-0.13533528323);
+}
+
 int main()
 {
-   cout<<y(0)<<endl;
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
    cout<<"Hello world!"<<endl;
    return 0;
 }
