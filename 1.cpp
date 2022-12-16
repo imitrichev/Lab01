d0111f73ee4681881a77cf68486cb3c33787e334
imitrichev <imitrichev@muctr.ru>
2022-12-16 16:51:04 +0300

    add 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math>
+
+//2. y=x^3 cos(x) - e^(2x)
+
+using namespace std
+
+double addend1(double x)
+{
+	return x*x*x + cos(x);
+}
+
+double addend2(double x)
+{
+	return exp(2*x);
+}
+double y(double x)
+{
+	return  addend1(x)-addend2(x);
+}
+
+int main()
+{
+   cout<<y(0)<<endl;
+   cout<<"Hello world!"<<endl;
+   return 0;
+}
